36d916f21fa9fc2e9d6ad69559e1f57559b5ef33
Armando <xxnowonxx@gmail.com>
2018-06-16 12:25:10 -0700

    file updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,25 +6,23 @@ function showDiv() {
     document.getElementById('welcomeDiv').style.display = "block";
 }
 
-var trivia = {
-    correct: 0,
-    incorrect: 0,
-    unanswered: 0,
-    currentSet: 0,
-}
+var correct = 0;
+var incorrect = 0;
+var unanswered = 0;
+    
 
 // create 25 second countdown
 // display Time Remaining: 25sec, start countdown, show countdown
 // create logic for start button
 
-var number = 25;
+var number = 5;
 
 var intervalId;
 
 function startGame() {
-    trivia.correct = 0;
-    trivia.incorrect= 0;
-    trivia.unanswered = 0;
+    correct = 0;
+    incorrect= 0;
+    unanswered = 0;
     clearInterval(intervalId);
     intervalId = setInterval(decrement, 1000);
     $("#start").hide();
@@ -40,7 +38,7 @@ function decrement() {
 
         stop();
 
-        $("#welcomeDiv").html("<p><h2>All Done</h2></p>").append("<p><h5>Correct Answers: " + trivia.correct + "</h5></p>").append("<p><h5>Wrong Answers: " + trivia.incorrect + "</h5></p>").append("<p><h5>Unanswered: " + trivia.unanswered + "</h5></p>")
+        $("#welcomeDiv").html("<p><h2>All Done</h2></p>").append("<p><h5>Correct Answers: " + correct + "</h5></p>").append("<p><h5>Wrong Answers: " + incorrect + "</h5></p>").append("<p><h5>Unanswered: " + unanswered + "</h5></p>")
     }
 }
 
@@ -51,22 +49,7 @@ function stop() {
 
 // create logic to check answers
 
-var correctAnswer = $.each($("input[id=answers]:checked").val() === "correct"); 
-
-// function results() {
-//     if (correctAnswer) {
-//         trivia.correct++;
-//     } else trivia.incorrect++
-// }
-
-var radios = document.querySelectorAll('input[type="radio"]:checked');
-
-console.log(radios);
-
-var value = radios.length>0? radios[0].value.name: null;
-
-if (radios.value === correctAnswer && radio.checked) {
-    console.log(value);
-    trivia.correct++;
-    console.log(radios.value);
+if ("#question", value="correct") {
+    trivia.correct++
 }
+
